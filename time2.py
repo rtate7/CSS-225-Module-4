0ca0c6dcf0266818a797f2655a3ddb4fb04c5cec
btate712 <btate712@gmail.com>
2021-01-22 08:45:24 -0600

    completed debugging time2.py

--- time2.py
+++ time2.py
@@ -1,7 +1,7 @@
-str_time = input("What time is it now?")
-str_wait_time = input("What is the number of nours to wait?")
+str_time = input("What time is it now? ")
+str_wait_time = input("What is the number of hours to wait? ")
 time = int(str_time)
-wai_time = int(str_wait_time)
+wait_time = int(str_wait_time)
 
-time_when_alarm_go_off = time + wait_time
+time_when_alarm_go_off = (time + wait_time) % 24
 print(time_when_alarm_go_off)
