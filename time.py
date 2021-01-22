6d7d8f72cd908f99d194d0f94b29c5cbb19370fd
btate712 <btate712@gmail.com>
2021-01-22 08:43:05 -0600

    completed debugging for time.py

--- time.py
+++ time.py
@@ -1,8 +1,8 @@
-currentTimeStr = input("What is the current time (in hours 0-23)?")
-waitTimeStr = input("How many hours do you want to wait"
+currentTimeStr = input("What is the current time (in hours 0-23)? ")
+waitTimeStr = input("How many hours do you want to wait? ")
 
-currentTimeInt = int(current_time_str)
-waitTimeInt = int(wait_time_str)
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
 
 finalTimeInt = currentTimeInt + waitTimeInt
-print(finalTime_Int)
+print(str(finalTimeInt))
