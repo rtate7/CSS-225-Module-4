e3332450221cacd8c039c77da5af68ee1e5017c2
btate712 <btate712@gmail.com>
2021-01-22 08:33:54 -0600

    debugging complete for collection.py

--- collection.py
+++ collection.py
@@ -9,12 +9,13 @@
 # Anthony Trollope, 1882
 # Gerard Manley Hopkins, 1889
 
-authrs = {
+authors = {
     "Charles Dickens": "1870",
     "William Thackeray": "1863",
     "Anthony Trollope": "1882",
     "Gerard Manley Hopkins": "1889"
-
-for author date in authors.items{}:
-    print "%s" % authors + " died in " + "%d." % Date
 }
+
+for author, date in authors.items():
+    print("%s" % author + " died in " + "%d." % int(date))
+
